39746db915d0d75561f55b4a375b7bf030d90840
Øystein Wergeland <OYSW@equinor.com>
2020-02-05 13:14:25 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+	name='xl',
+	packages=['xl'],
+	version='0.0.1',
+	test_suite='tests',
+	install_requires=['pandas','xlrd'],
+	entry_points={'console_scripts': ['xl=xl:main']},
+)
---
+++ xl/__init__.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def read_excel(fname):
+	return pd.read_excel(fname)
+
+def main():
+	pass
+
